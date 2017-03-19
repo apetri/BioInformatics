59868b16bc8ab8b2520772f4ef6fcf66bdc7a016
apetri <apetri@phys.columbia.edu>
2017-03-19 18:39:11 -0400

    clean up laquire, background.m is unnecessary

--- laquire.m
+++ laquire.m
@@ -1,59 +1,43 @@
-numFiles = 10;
-%range = 'L2:L601';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%Options%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+numRealizations = 10;
 sheet = 1;
 
-myData = cell(1,numFiles);
-myF = cell(1,numFiles);
-mydF = cell(1,numFiles);
-t1= [.1:.1:60]
-t=transpose(t1)
+%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%Data%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%
 
-for fileNum = 1:numFiles
+myData = cell(1,numRealizations);
+myF = cell(1,numRealizations);
+mydF = cell(1,numRealizations);
+myClean = cell(1,numRealizations);
+
+for r = 1:numRealizations
     fileName = 'data/a2.xlsx';
-    myData{fileNum} = soma(fileName,sheet,'B'+fileNum,2,601);
+    myData{r} = soma(fileName,sheet,'B'+r,2,601);
+    myF{r} = mean(myData{r}(100:200));
+    myDf{r} = myData{r}/myF{r} - 1.0
 end
 
-for fileNum = 1:numFiles
-    dump= myData{fileNum}
-    dumpy=dump(100:200)
-    F=mean(dumpy)
-    myF{fileNum} = F;
-end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%Background%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-for fileNum = 1:numFiles
-    dump = myData{fileNum}
-    dumpy = myF{fileNum}
-    deltaF = dump-dumpy
-    mydF{fileNum} = deltaF/F
-end
+myBackground = cell(1,numRealizations);
 
-%range = 'B102:B501';
-%sheet = 1;
-myBackground = cell(1,numFiles);
-
-for fileNum = 1:numFiles
+for r = 1:numRealizations
     fileName = 'data/a2.xlsx';
-    myBackground{fileNum} = background(fileName,sheet,'B2:B601');
+    myBackground{r} = soma(fileName,sheet,'B',2,601);
+    myClean{r} = myData{r} - myBackground{r};
+    myF{r} = mean(myClean{r}(100:200))
+    mydF{r} = myClean{r}/myF{r} - 1.0
 end
 
-for fileNum = 1:numFiles
-    dump = myData{fileNum}
-    dumpy = myBackground{fileNum}
-    myclean{fileNum} = dump-dumpy
-    end
-
-for fileNum = 1:numFiles
-    dump= myclean{fileNum}
-    dumpy=dump(100:200)
-    F=mean(dumpy)
-    myF{fileNum} = F
-end
-for fileNum = 1:numFiles
-    dump = myclean{fileNum}
-    dumpy = myF{fileNum}
-    deltaF = dump-dumpy
-    mydF{fileNum} = deltaF/dumpy
-end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%Plotting%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 dfof = cell2mat(mydF);
 average = mean(dfof,2);
