0488e10b7b3587346b87398a30fc91b00921f1ab
apetri <apetri@phys.columbia.edu>
2017-03-20 09:22:50 -0400

    allow multiple files in input

--- laquire.m
+++ laquire.m
@@ -2,37 +2,42 @@
 %%%%%%%%%%Options%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-numRealizations = 10;
-sheet = 1;
+%Each input row must be in the format: <fileName> <sheet> <background column> <first data column> <last data column> <first row> <last row>
+
+inputs = {
+    
+    'data/a2.xlsx' 1 'B' 'C' 'D' 2 601 ;
+    'data/a2.xlsx' 1 'B' 'E' 'G' 2 601 ;
+
+} 
 
 %%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%Data%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%
 
-myData = cell(1,numRealizations);
-myF = cell(1,numRealizations);
-mydF = cell(1,numRealizations);
-myClean = cell(1,numRealizations);
+%Time: watch out that with this choice length(t)=600. lastRow - firtRow in the data must match this 
+t = transpose([.1:.1:60]);
 
-for r = 1:numRealizations
-    fileName = 'data/a2.xlsx';
-    myData{r} = excelRead(fileName,sheet,'B'+r,2,601);
-    myF{r} = mean(myData{r}(100:200));
-    mydF{r} = myData{r}/myF{r} - 1.0
-end
+%Counter start
+r = 1;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%Background%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Cycle over files
+for f = 1:size(inputs,1)
 
-myBackground = cell(1,numRealizations);
+    [ fileName sheet bCol firstCol lastCol firstRow lastRow ] = inputs{f,:} ;
+    
+    %Load background
+    myBackground = excelRead(fileName,sheet,bCol,firstRow,lastRow) ;
 
-for r = 1:numRealizations
-    fileName = 'data/a2.xlsx';
-    myBackground{r} = excelRead(fileName,sheet,'B',2,601);
-    myClean{r} = myData{r} - myBackground{r};
-    myF{r} = mean(myClean{r}(100:200))
-    mydF{r} = myClean{r}/myF{r} - 1.0
+    %Load data, subtract background
+    for col = firstCol:lastCol
+        myData{r} = excelRead(fileName,sheet,col,firstRow,lastRow) ;
+        myClean{r} = myData{r} - myBackground ;
+        myF{r} = mean(myClean{r}(100:200)) ;
+        mydF{r} = myClean{r}/myF{r} - 1.0 ;
+        r = r+1 ;
+    end 
+
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -41,4 +46,5 @@ end
 
 dfof = cell2mat(mydF);
 average = mean(dfof,2);
-plot(t,dfof);
+plot(t,dfof); hold;
+plot(t,average,'k--');
